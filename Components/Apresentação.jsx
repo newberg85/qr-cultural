4371335729ac16361c45e9a37f3f3ff10c677e16
Letycia Santos <letycia09santos@gmail.com>
2025-09-22 12:52:41 -0300

    criação do footer e da header

---
+++ Components/Apresentação.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Apresentação = () => {
+  return (
+    <div className="flex justify-center items-center">
+        <h1 className="text-2xl text-[#1B7E44]">
+            Qr cultural
+        </h1>
+        <p>
+            Conheça Maranguape como você nunca viu!
+        </p>
+    </div>
+  )
+}
+
+export default Apresentação
