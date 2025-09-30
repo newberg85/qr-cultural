9e5e256e32b6b30983ad46cda7397c8fd478e0f7
Arimateia <sousaarimateia4567@gmail.com>
2025-09-30 16:02:48 -0300

    Conexão com o firebase

---
+++ app/services/firebaseConnection.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2HIdWKhsjXu182gWU6ApXt4yDyMgiYjw",
+  authDomain: "qr-cultural-e8e71.firebaseapp.com",
+  projectId: "qr-cultural-e8e71",
+  storageBucket: "qr-cultural-e8e71.firebasestorage.app",
+  messagingSenderId: "53519925285",
+  appId: "1:53519925285:web:b09bd7227ff8c4a8c13fcc"
+};
+
+
+const app = initializeApp(firebaseConfig);
