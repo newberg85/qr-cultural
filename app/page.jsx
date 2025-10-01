2dcdd48a24b6699c507a3d20f42234ce4ecdd531
Arimateia <sousaarimateia4567@gmail.com>
2025-10-01 15:52:39 -0300

    Tratamentos iniciais com o banco de dados

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,14 @@
-import Header from "@/Components/Header";
-import Footer from "@/Components/Footer";
-import Image from "next/image";
 import Apresentação from "@/Components/Apresentação";
+import Footer from "@/Components/Footer";
+import Header from "@/Components/Header";
 import Imagens from "@/Components/Imagens";
+
 
 export default function Home() {
   return (
     <div className="font-sans">
       <Header />
-      <Apresentação />
+      <Apresentação/>
       <Imagens />
       <Footer />
     </div>
